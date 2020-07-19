4a6f5d422637af8647e3a44bf0f2b5de36462d63
Diego <dhnogueira1@hotmail.com>
2020-07-19 15:03:33 -0300

    Backend finalizado

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+const express = require ('express');
+const server = express();
+
+server.use(express.json());
+
+
+
+const TaskRouter = require('./routes/TaskRoutes');
+
+server.use('/task', TaskRouter);
+
+server.listen(3000, () => {
+    console.log ("Api ONLINE");
+});
